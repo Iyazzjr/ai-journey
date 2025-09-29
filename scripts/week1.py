f5d07968a846a73dcf386334728f61950a37ee61
Your Name <your-github-email@example.com>
2025-09-29 21:24:12 +0100

    feat: initial workspace with venv, folders, README

---
+++ scripts/week1.py
@@ -0,0 +1,15 @@
+name = "Iyazz"
+print("My name is", name)
+
+x, y = 7, 12
+print("Before swap:", x, y)
+x, y = y, x
+print("After swap:", x, y)
+
+t = int(input("enter first number:"))
+p = int(input("enter second number:"))
+print("sum: ", t + p)
+print("difference: ", t - p)
+print("product: ", t * p)
+print("Quotient: ", t / p)
+
